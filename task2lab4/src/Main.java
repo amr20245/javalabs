c5b4b1af0ab5a020f946ef3401a159b83e185db1
amr20245 <120528553+amr20245@users.noreply.github.com>
2024-09-16 14:54:58 -0400

    Add files via upload

---
+++ task2lab4/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 150.00;
+        double summerCost = 200.00;
+        double fallCost = 100.00;
+        double winterCost = 120.00;
+
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        // Output the results
+        System.out.println("Spring maintenance cost: $" + springCost);
+        System.out.println("Summer maintenance cost: $" + summerCost);
+        System.out.println("Fall maintenance cost: $" + fallCost);
+        System.out.println("Winter maintenance cost: $" + winterCost);
+        System.out.println("Total yearly maintenance cost is: $" + totalCost);
+    }
+}
