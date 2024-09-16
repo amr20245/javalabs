391220b1ee0760d4439fcb0bccddf7f86a6e56fa
amr20245 <120528553+amr20245@users.noreply.github.com>
2024-09-16 15:02:18 -0400

    Add files via upload

---
+++ task4lab4/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 4;  // You can change this to any number
+
+        // Check if the number is even or odd
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+    }
+}
